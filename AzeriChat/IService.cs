fcab6fb7bfd5646baf7a1dbd804eb6bbbf9cefb8
Cavid <mehsumovcavid@gmail.com>
2021-07-04 17:45:24 +0400

    Done Task

---
+++ AzeriChat/IService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AzeriChat
+{
+    static public class IService
+    {
+       static public string GetResponseForSalam()
+        {
+            List<string>SalamRequest = new List<string>
+            {
+                "Salam","Aleykum Salam","Salam Aleykum"
+            };
+
+            Random random = new Random();
+            int index = random.Next(SalamRequest.Count);
+            return SalamRequest[index];
+
+        }
+        static public string GetResponseForNecesen()
+        {
+            List<string> NecesenRequest = new List<string>
+            {
+                "Sagol.Sen Necesen?","Sukur Sen Necesen?","Yaxsi"
+            };
+
+            Random random = new Random();
+            int index = random.Next(NecesenRequest.Count);
+            return NecesenRequest[index];
+
+        }
+        static public string GetResponseForHardasan()
+        {
+            List<string> HardasanRequest = new List<string>
+            {
+                "Evde","Ishde","Colde"
+            };
+
+            Random random = new Random();
+            int index = random.Next(HardasanRequest.Count);
+            return HardasanRequest[index];
+
+        }
+        static public string GetResponseForNeyneyirsen()
+        {
+            List<string> NeyneyirsenRequest = new List<string>
+            {
+                "Yatiram","Kinoya Baxiram","Kod Yaziram","Oyun Oynayiram"
+            };
+
+            Random random = new Random();
+            int index = random.Next(NeyneyirsenRequest.Count);
+            return NeyneyirsenRequest[index];
+
+        }
+        static public string GetResponseRandom()
+        {
+            List<string> NeyneyirsenRequest = new List<string>
+            {
+                "IdmanDa","Seherde","Sagol","2 deq catiram"
+            };
+
+            Random random = new Random();
+            int index = random.Next(NeyneyirsenRequest.Count);
+            return NeyneyirsenRequest[index];
+
+        }
+    }
+}
--- AzeriChat/MainWindow.xaml.cs
+++ AzeriChat/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,77 @@ namespace AzeriChat
     /// </summary>
     public partial class MainWindow : Window
     {
+     
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string spac = "\n";
+           
+
+
+            ListBox1.HorizontalAlignment = HorizontalAlignment.Left;
+            ListBox1.Items.Add(MessageTxtBox.Text);
+            ListBox1.Items.Add(spac);
+
+            MessageTxtBox.Text = MessageTxtBox.Text.ToLower();
+            if (MessageTxtBox.Text == "salam")
+            {
+                ListBox.HorizontalAlignment = HorizontalAlignment.Right;
+                ListBox.Items.Add(spac);
+                ListBox.Items.Add(IService.GetResponseForSalam());
+            }
+            else if (MessageTxtBox.Text == "necesen")
+            {
+                ListBox.HorizontalAlignment = HorizontalAlignment.Right;
+                ListBox.Items.Add(spac);
+                ListBox.Items.Add(IService.GetResponseForNecesen());
+            }
+            else if (MessageTxtBox.Text == "neyneyirsen")
+            {
+                ListBox.HorizontalAlignment = HorizontalAlignment.Right;
+                ListBox.Items.Add(spac);
+                ListBox.Items.Add(IService.GetResponseForNeyneyirsen());
+            }
+            else if (MessageTxtBox.Text == "hardasan")
+            {
+                ListBox.HorizontalAlignment = HorizontalAlignment.Right;
+                ListBox.Items.Add(spac);
+                ListBox.Items.Add(IService.GetResponseForHardasan());
+            }
+            else
+            {
+                ListBox.HorizontalAlignment = HorizontalAlignment.Right;
+                ListBox.Items.Add(spac);
+                ListBox.Items.Add(IService.GetResponseRandom());
+            }
+            MessageTxtBox.Text = string.Empty;
+
+
+
+        }
+
+
+        private void PinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+
+
+            Nullable<bool> result = openFileDlg.ShowDialog();
+
+            if (result == true)
+            {
+                using (StringReader reader = new StringReader(openFileDlg.FileName))
+                {
+                    string label = reader.ReadToEnd();
+                    MessageTxtBox.Text = File.ReadAllText(label);
+
+                }
+            }
         }
     }
 }
